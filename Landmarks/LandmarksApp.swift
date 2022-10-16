2ac46d5a39729df741c67a33474d792b4b064ad1
rodolfo comparsi <rodolfocomparsi@hotmail.com>
2022-10-16 10:20:52 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rodolfo Comparsi on 16/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
