07beeabc3890655256de583bbe7957763c11fc30
rodolfo comparsi <rodolfocomparsi@hotmail.com>
2022-11-04 08:10:07 -0300

    add hexagonal params

---
+++ Landmarks/Helpers/FavoriteButton.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Rodolfo Comparsi on 18/10/22.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+        
+        
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
